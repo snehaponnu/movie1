f9b0e7e38ebc22df2d4bc8c34679a0e98ac7100a
snehachandran <snehachandran200@gmail.com>
2023-09-17 11:42:39 +0530

    hello

---
+++ movie_project/movieapp2/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+
+from movieapp2.models import Movie
+
+# Register your models here.
+admin.site.register(Movie)
